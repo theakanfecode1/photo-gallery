8a5aa78281d04ce9054624a6aa5f3ba9d69b8d8e
Daniel Ogundiran <56872396+theakanfecode1@users.noreply.github.com>
2023-08-14 14:50:39 +0100

    Update api_key.dart

--- lib/api_key.dart
+++ lib/api_key.dart
@@ -1,9 +1,6 @@
-/*
-API_KEY to access unsplash API. When deploying to store obfuscation
-will be enabled making it almost for hackers to break into the app
- */
 
-const String unsplashApiKey = '30HFl8YDJTeyPLVm4IfWstURr8wQqSvRmzq-ZdzQMr0';
+
+const String unsplashApiKey = 'insert your api key';
 
 
 
